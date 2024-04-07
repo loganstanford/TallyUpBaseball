40a6400ac7c346313df5db437271eb3bf49cfaff
Logan Stanford <logan@onscripttech.com>
2024-04-07 01:54:30 -0400

    Init

---
+++ data/refresh-agg-tables.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, text
+from config import DB_CONNECTION_STRING
+
+tables = ["agg_stats_ytd",
+          "agg_stats_yesterday",
+          "agg_stats_last30",
+          "agg_stats_last15",
+          "agg_stats_last7",
+          "agg_stats_last3"]
+
+engine = create_engine(DB_CONNECTION_STRING)
+
+with engine.connect() as connection:
+    for table in tables:
+        truncate_query = text(f"TRUNCATE TABLE {table}")
+        connection.execute(truncate_query)
+
+        # Correct way to call a stored procedure
+        call_proc = text(f"CALL `update-{table}`()")
+        connection.execute(call_proc)
+
+    connection.commit()
